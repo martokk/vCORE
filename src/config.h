9a88aeba2a7075bda23334a2775f2e5e227628df
Martokk <techv76@gmail.com>
2017-01-30 05:21:43 -0600

    CONFIG

--- src/config.h
+++ src/config.h
@@ -14,18 +14,18 @@ vCORE Universal IoT Device v0.1.1
 
 
 /********** DEVICE ***************************************/
-  #define DEVICE_NAME               "tables"   // Must be unique on Network.
+  #define DEVICE_NAME               "change_me"   // Must be unique on Network.
 
 /********** PUSH BUTTON ***************************************/
-  #define TOTAL_RESET_BUTTONS       1         // 0 or 1 Buttons to reset device
-  #define RESET_BUTTON_PIN          D7        // Reset Button Pin
+  #define TOTAL_RESET_BUTTONS       0         // 0 or 1 Buttons to reset device
+  #define RESET_BUTTON_PIN          D0        // Reset Button Pin
 
 /********** LEDs ***************************************/
-  #define TOTAL_LED_STRIPS          1         // Supports up to 5 seperate LED Strips
-  #define LED_BRIGHTNESS_LIMIT      200       // Limit Brightnes Level (Max = 255)
+  #define TOTAL_LED_STRIPS          0         // Supports up to 5 seperate LED Strips
+  #define LED_BRIGHTNESS_LIMIT      50       // Limit Brightnes Level (Max = 255)
 
-  #define LED1_PIN                  D1        // Arduno pin
-  #define LED1_COUNT                158       // Total LEDs
+  #define LED1_PIN                  D0        // Arduno pin
+  #define LED1_COUNT                0         // Total LEDs
   // #define LED1_MQTT_MODE            0      // 0 = Use LED1; 1 = Use own
 
 /********** PIR ***************************************/
@@ -34,49 +34,49 @@ vCORE Universal IoT Device v0.1.1
   #define PIR_MQTT_RETRIGGER_DELAY  4000      // Re-send PIR HIGH status to MQTT
   #define PIR_CALIBRATION_DELAY     15000     // Calibration time
 
-  #define PIR1_PIN                  D8        // Arduiuno Pin
+  #define PIR1_PIN                  D0        // Arduiuno Pin
   #define PIR1_REARM_MODE           0         // 0 = Use PIR1; 1 = Use own
   #define PIR1_REARM_DELAY          3000      // Time before PIR triggers back to 0 ("off")
 
-  #define PIR2_PIN                  D8        // Arduno pin
+  #define PIR2_PIN                  D0        // Arduno pin
   #define PIR2_REARM_MODE           0         // 0 = Use PIR1; 1 = Use own
   #define PIR2_REARM_DELAY          3000      // Time before PIR triggers back to 0 ("off")
 
-  #define PIR3_PIN                  D8        // Arduno pin
+  #define PIR3_PIN                  D0        // Arduno pin
   #define PIR3_REARM_MODE           1         // 0 = Use PIR1; 1 = Use own
   #define PIR3_REARM_DELAY          5000      // Time before PIR triggers back to 0 ("off")
 
 /********** DHT ***************************************/
-  #define TOTAL_DHT_SENSORS         1
-  #define DHT_PIN                   D4        // Arduno pin
-  #define DHT_TYPE                  DHT22     // DH11; DH22; DHT21; AM2301
+  #define TOTAL_DHT_SENSORS         0
+  #define DHT_PIN                   D0        // Arduno pin
+  #define DHT_TYPE                  DHT11     // DH11; DH22; DHT21; AM2301
   #define DHT_INTERVAL_LOOP         3000      // DHT11=1000; DHT22=2000
   #define DHT_MQTT_PUB_INTERVAL     120000    // Delay between MQTT Publish (Avereged every PUB_INTERVAL)
   #define DHT_TEMP_OFFSET           0         // Calibration offset
 
 /********** MQTT ***************************************/
   /*--------- DEVICE --------------*/
-    #define PUB_DEVICE              "device/tables/device"
-    #define PUB_DEBUG               "device/tables/debug"
+    #define PUB_DEVICE              "device/change_me/device"
+    #define PUB_DEBUG               "device/change_me/debug"
 
   /*--------- PIR --------------*/
-    #define PUB_PIR1                "device/tables/pir1"
-    #define PUB_PIR2                "device/tables/pir2"
-    #define PUB_PIR3                "device/tables/pir3"
+    #define PUB_PIR1                "device/change_me/pir1"
+    #define PUB_PIR2                "device/change_me/pir2"
+    #define PUB_PIR3                "device/change_me/pir3"
 
   /*--------- DHT --------------*/
-    #define PUB_HUMIDITY            "device/tables/humidity"
-    #define PUB_TEMPERATURE         "device/tables/temperature"
+    #define PUB_HUMIDITY            "device/change_me/humidity"
+    #define PUB_TEMPERATURE         "device/change_me/temperature"
 
   /*--------- LED STRIP 1 --------------*/
-    #define SUB_LED1_POWER          "device/tables/led1/power"
-    #define SUB_LED1_COLOR          "device/tables/led1/color"
-    #define SUB_LED1_BRIGHTNESS     "device/tables/led1/brightness"
-    #define SUB_LED1_EFFECT         "device/tables/led1/effect"
-    #define SUB_LED1_SPEED          "device/tables/led1/speed"
+    #define SUB_LED1_POWER          "device/change_me/led1/power"
+    #define SUB_LED1_COLOR          "device/change_me/led1/color"
+    #define SUB_LED1_BRIGHTNESS     "device/change_me/led1/brightness"
+    #define SUB_LED1_EFFECT         "device/change_me/led1/effect"
+    #define SUB_LED1_SPEED          "device/change_me/led1/speed"
 
-    #define PUB_LED1_POWER          "device/tables/led1/power/pub"
-    #define PUB_LED1_COLOR          "device/tables/led1/color/pub"
-    #define PUB_LED1_BRIGHTNESS     "device/tables/led1/brightness/pub"
-    #define PUB_LED1_EFFECT         "device/tables/led1/effect/pub"
-    #define PUB_LED1_SPEED          "device/tables/led1/speed/pub"
+    #define PUB_LED1_POWER          "device/change_me/led1/power/pub"
+    #define PUB_LED1_COLOR          "device/change_me/led1/color/pub"
+    #define PUB_LED1_BRIGHTNESS     "device/change_me/led1/brightness/pub"
+    #define PUB_LED1_EFFECT         "device/change_me/led1/effect/pub"
+    #define PUB_LED1_SPEED          "device/change_me/led1/speed/pub"
